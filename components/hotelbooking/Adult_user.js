2083a248cdd76cfbfd7cedcdae389f6aa9d83162
EvgenijB <evgenij.baltais@gmail.com>
2022-10-17 13:00:56 +0300

    1

--- components/hotelbooking/Adult_user.js
+++ components/hotelbooking/Adult_user.js
@@ -2,6 +2,21 @@ import styles from "/styles/hotelbooking/Adult_user.module.css"
 
 export default function Adult_user ({number}) {
 
+    function chooseData () {
+
+        console.log(event.target)
+
+        event.target.parentElement.parentElement.classList.contains('opened-list') && event.target.classList.contains('hotel-bron-clickarea') ?
+        event.target.parentElement.parentElement.classList.remove('opened-list') :
+        event.target.parentElement.parentElement.classList.add('opened-list')
+    }
+
+    function chooseVariant () {
+        console.log(event.target.innerText)
+        event.target.parentElement.querySelector('.hotel-bron-inside__default').innerText = event.target.innerText
+        event.target.parentElement.parentElement.parentElement.classList.remove('opened-list')
+    }
+
     return (
         <div className={styles["hotel-bron-ready__guest"]}>
             <p className={styles["guest-text-title"]}>Гость {number} <span>(покупатель)</span></p>
@@ -18,23 +33,25 @@ export default function Adult_user ({number}) {
                         ${styles["hotel-bron-input-patronymic"]}`}
                     placeholder = "Отчество" />
                 </div>
-                <div className={`${styles["hotel-bron-input-w"]} ${styles["hotel-bron-necessarily"]}`}>
-                    <div className={`${styles["hotel-bron-block"]} ${styles["hotel-bron-input-citizenship"]}`}>
-                        <div className={styles["hotel-bron-input-arrow"]}></div>
+                <div className={`${styles["hotel-bron-input-w"]} ${styles["hotel-bron-necessarily"]} hotel-bron-citizenship`} onClick = {chooseData}>
+                    <div className={`${styles["hotel-bron-block"]} ${styles["hotel-bron-input-citizenship"]} hotel-bron-input-citizenship`}>
+                        <div className={`${styles["hotel-bron-clickarea"]} hotel-bron-clickarea`}></div>
+                        <div className={`${styles["hotel-bron-input-arrow"]} hotel-bron-input-arrow`}></div>
                         <div className={styles["hotel-bron-inside"]}>
-                            <div className = {`${styles["hotel-bron-inside__item"]} ${styles["hotel-bron-inside__default"]}`}>Гражданство</div>
-                            <div className={styles["hotel-bron-inside__item"]}>Российская Федерация</div>
-                            <div className={styles["hotel-bron-inside__item"]}>Иное</div>
+                            <div className = {`${styles["hotel-bron-inside__item"]} ${styles["hotel-bron-inside__default"]} hotel-bron-inside__default`}>Гражданство</div>
+                            <div className={`${styles["hotel-bron-inside__item"]} hotel-bron-inside__item`} onClick = {chooseVariant}>Российская Федерация</div>
+                            <div className={`${styles["hotel-bron-inside__item"]} hotel-bron-inside__item`} onClick = {chooseVariant}>Иное</div>
                         </div>
                     </div>
                 </div>
-                <div className={`${styles["hotel-bron-input-w"]} ${styles["hotel-bron-necessarily"]}`}>
-                    <div className={`${styles["hotel-bron-block"]} ${styles["hotel-bron-input-sex"]}`}>
-                        <div className={styles["hotel-bron-input-arrow"]}></div>
+                <div className={`${styles["hotel-bron-input-w"]} ${styles["hotel-bron-necessarily"]} hotel-bron-sex`} onClick = {chooseData}>
+                    <div className={`${styles["hotel-bron-block"]} ${styles["hotel-bron-input-sex"]} hotel-bron-input-sex`}>
+                        <div className={`${styles["hotel-bron-clickarea"]} hotel-bron-clickarea`}></div>
+                        <div className={`${styles["hotel-bron-input-arrow"]} hotel-bron-input-arrow`}></div>
                         <div className={styles["hotel-bron-inside"]}>
-                            <div className = {`${styles["hotel-bron-inside__item"]} ${styles["hotel-bron-inside__default"]}`}>Пол</div>
-                            <div className={styles["hotel-bron-inside__item"]}>Мужской</div>
-                            <div className={styles["hotel-bron-inside__item"]}>Женский</div>
+                            <div className = {`${styles["hotel-bron-inside__item"]} ${styles["hotel-bron-inside__default"]} hotel-bron-inside__default`}>Пол</div>
+                            <div className={styles["hotel-bron-inside__item"]} onClick = {chooseVariant}>Мужской</div>
+                            <div className={styles["hotel-bron-inside__item"]} onClick = {chooseVariant}>Женский</div>
                         </div>
                     </div>
                 </div>
