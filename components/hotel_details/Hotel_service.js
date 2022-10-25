23dafc3c96cb97d51370ee16e1f2daefbcd5bd95
Evgeny <evgenij.baltais@gmail.com>
2022-10-25 14:10:35 +0300

    1

--- components/hotel_details/Hotel_service.js
+++ components/hotel_details/Hotel_service.js
@@ -1,13 +1,11 @@
 import styles from "../../styles/Hoteldetail.module.css"
 
 const Hotel_service = ({services}) => {
-    console.log(services)
     return (
         <div className={styles["result-description"]}>
             <h2 className = "section-title icon-item icon-item-serv">Услуги</h2>
             <div className = {`${styles["hotel-title-section"]} ${styles["hotel-serv-descr"]}`}>
                 {services.map((item, index) => {
-                    console.log(item.amenities)
                     return item.amenities.length > 0 ? (
                         <div key = {index}className={styles["serv-type-section"]}>
                             <h3>{item.group_name}</h3>
