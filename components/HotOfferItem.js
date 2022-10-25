f96e5f84033ba71bc2d3e7858378e420c36b36de
Evgeny <evgenij.baltais@gmail.com>
2022-10-25 17:25:11 +0300

    1

--- components/HotOfferItem.js
+++ components/HotOfferItem.js
@@ -3,6 +3,12 @@ import styles from "../styles/HotOffersItem.module.css"
 
 const HotOfferItem = ({item}) => {
 
+    let stars = []
+
+    for (let i = 0; i < item.star_rating; i++) {
+        stars.push(i)
+    }
+
     return (
         <Link href = {`/hoteldetail?hotel-id=${item.id}`}>
             <a className = {styles["hot-deal-item"]}>
@@ -11,8 +17,13 @@ const HotOfferItem = ({item}) => {
                     <div>
                         <p className = {styles["hot-deal-title"]}>{item.name}</p>
                         <p className = {styles["hot-deal-text"]}>{item.adress}</p>
-                        <div>
 
+                        <div className = {styles["hot-stars-container-w"]}>
+                            {stars.map((item, index) => {
+                                return (
+                                    <div key = {index} src = "/public/images/star.svg" className = {`${styles["hot-deal-star"]} ${styles["hot-deal-star-gold"]}`}></div>
+                                )
+                            })}
                         </div>
                     </div>
                     <div className = {styles["hot-deal-price"]}>
